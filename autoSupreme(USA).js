230036726ca0d0ba77871e3f004cd8f572688346
Chrisds7 <59126039+Chrisds7@users.noreply.github.com>
2020-06-09 16:16:07 -0400

    Update autoSupreme(USA).js

--- autoSupreme(USA).js
+++ autoSupreme(USA).js
@@ -6,15 +6,18 @@ javascript: (function (){
     }
     autoFill("order_billing_name", "Chris Smith");
     autoFill("order_email", "insertEmail@gmail.ca");
-    autoFill("order_tel", "4167896650");
-    autoFill("bo", "39 Dewey Dr");
+    autoFill("order_tel", "InsertPhoneNumber");
+    autoFill("bo", "Insert Address");
     autoFill("order_billing_zip", "99501");
     autoFill("order_billing_city", "Anchorage");
     document.getElementById("order_billing_country").value = "USA";
     document.getElementById("order_billing_state").value = "AK";
-    autoFill("rnsnckrn", "4060511703000022");
-    document.getElementById("credit_card_month").value = "01";
-    document.getElementById("credit_card_year").value = "2021";
+    autoFill("rnsnckrn", "InsertCreditCardNumber");
+    document.getElementById("credit_card_month").value = "00";
+    // insert a value for the expiry month
+    document.getElementById("credit_card_year").value = "2020";
+    // insert a value for the expiry year
     autoFill("orcer", "116");
+    // insert a value for the cvv
     document.getElementsByClassName("icheckbox_minimal")[1].getElementsByClassName("iCheck-helper")[0].click()
 })();
